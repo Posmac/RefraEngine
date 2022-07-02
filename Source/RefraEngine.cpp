35a6a5870a2b882a3455ec997946e326288a013b
Posmac <nicolai.posmac@gmail.com>
2022-07-02 22:01:34 +0300

    Created first window

--- Source/RefraEngine.cpp
+++ Source/RefraEngine.cpp
@@ -1,6 +1,61 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cout << "Initialization failed" << '\n';
+        glfwTerminate();
+        return -1;
+    }
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
+                                          "Refra Engine", NULL, NULL);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    if (!window)
+    {
+        std::cout << "Window creation failed" << '\n';
+        glfwTerminate();
+        return -1;
+        // Window or OpenGL context creation failed
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0, 1, 1, 1);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
