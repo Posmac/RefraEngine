868ce315d9c0210a139f704748e7439f04d936f3
Posmac <nicolai.posmac@gmail.com>
2022-07-04 10:39:48 +0300

    Added render empty class

---
+++ Headers/Renderer.h
@@ -0,0 +1,19 @@
+#ifndef REFRAENGINE_RENDERER_H
+#define REFRAENGINE_RENDERER_H
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace rfe
+{
+    class Renderer
+    {
+
+    };
+}
+
+#endif //REFRAENGINE_RENDERER_H
